bf7c6c31c9240b8bee15ab78410290e2a19abeaa
Divyanshi yadav <67509076+sap-phire@users.noreply.github.com>
2021-08-17 09:57:23 +0530

    Add files via upload

---
+++ public/js/api.js
@@ -0,0 +1,8 @@
+let api_key="fbf33356138be0429e9669c0b51597cb";
+
+let img_url="https://image.tmdb.org/t/p/w500";
+let original_img_url="https://image.tmdb.org/t/p/original";
+let genres_list_http="https://api.themoviedb.org/3/genre/movie/list?";
+
+let movie_genres_http="https://api.themoviedb.org/3/discover/movie?";
+let movie_detail_http="https://api.themoviedb.org/3/movie";
